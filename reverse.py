1133e7ebfb7a89af37a6844f1e80fbeafbeb7c5a
Mehmet Tunahan Okumuş <80225796+mehmettunahanokumus@users.noreply.github.com>
2022-03-23 18:29:40 +0300

    Add files via upload

---
+++ reverse.py
@@ -0,0 +1,7 @@
+def reving_list(_list): #reversing using slicing
+    revd_list = _list[::-1] #reversed slicing method
+    return revd_list
+
+def rev_list(_list_): #reversing using built-in method
+    rd_list = _list_.reverse() #using method
+    return rd_list
